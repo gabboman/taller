a2752bb0f44454a994ded1fa74087515f752f465
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2015-07-16 18:01:47 +0200

    iniciando...

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Coche)
+admin.site.register(Trabajador)
+admin.site.register(Trabajo)
+admin.site.register(Pieza)
