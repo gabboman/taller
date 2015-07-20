acf5b829f2c274895d0ebda1b37392126c7fc7ca
Manuel-Jesus <manueljesusbeginesgonzalez@gmail.com>
2015-07-20 21:50:16 +0200

    añadida funciones para las clases

--- gestion/models.py
+++ gestion/models.py
@@ -24,6 +24,8 @@ class Trabajadores(models.Model):
     nombre = models.CharField(max_length=50)
     sueldo_hora = models.FloatField()
     #es el estipulado en el contrato no el que producen según la aplicacion
+    def __str__(self):
+        return self.nombre
 
     class Meta:
         verbose_name_plural = "Trabajadores"
@@ -35,6 +37,8 @@ class Piezas(models.Model):
     nombre_pieza = models.CharField(max_length=200)
     precio = models.FloatField()
     fecha_entrada = models.DateTimeField('fecha_entrada_pieza')
+    def __str__(self):
+        return self.nombre_pieza
 
     class Meta:
         verbose_name_plural = "Piezas"
@@ -50,11 +54,13 @@ class Trabajos(models.Model):
     descripcion = models.CharField(max_length=1000)
     hora_inicio = models.TimeField()
     hora_final = models.TimeField()
+    def __str__(self):
+        return self.fecha_trabajo
 
     class Meta:
         verbose_name_plural = "Trabajos"
         verbose_name = "Trabajos"
-#El eje central de la aplicacion son los trabajos, es un trabajo/tarea
+#El eje central de la aplicacion son los trabajofnames, es un trabajo/tarea
 
 
 class Proveedores(models.Model):
@@ -62,8 +68,16 @@ class Proveedores(models.Model):
     nombreempresa = models.CharField(max_length = 200)
     direccion = models.CharField(max_length = 200)
     telefono = models.IntegerField()
+    def __str__(self):
+        return self.nombreempresa
 
     class Meta:
         verbose_name_plural = "Proveedores"
         verbose_name = "Proveedores"
     # Añadimos proveedores con la infomacion  necesaria de cada uno de ellos
+
+
+
+#para las funciones de cada una de las clases se debe de poner un campo que intifique a cada clase como unica o poner una clave primaria
+
+#debemos de solucionar los nuemero de telefono para que solo se admintan numero de telefono y no nuemeros enteros
