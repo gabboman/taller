cfa51e7e72ec706cda7bf6c980cf89f8f9d4b1d1
Manuel-Jesus <manueljesusbeginesgonzalez@gmail.com>
2015-07-30 20:27:20 +0200

    Se ha puesto que se miestren todos los coches en la pagina de inicio

--- gestion/views.py
+++ gestion/views.py
@@ -5,7 +5,7 @@ from .models import Coches
 from django.http import HttpResponse
 
 def index(request):
-    Ultimos5Coches = Coches.objects.order_by('-fecha_entrada')[:5]#Devuelve los coches ordenados por fecha de entrada, del mas nuevo al mas viejo (sin el - seria del mas viejo al mas nuevo)
+    Ultimos5Coches = Coches.objects.order_by('-fecha_entrada')#Devuelve los coches ordenados por fecha de entrada, del mas nuevo al mas viejo (sin el - seria del mas viejo al mas nuevo)
     output = ', '.join([p.__str__() for p in Ultimos5Coches])#Los convierte a string
     return HttpResponse(output)#muestra el string en pantalla
 #Con esto podemos realizar cálculos. Es necesario investigar acerca del html, ya que así enviamos solo texto sin html
