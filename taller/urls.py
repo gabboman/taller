57c655ee22c6c8ef2cf4ef0f21cc032578cc3501
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2015-07-30 02:02:08 +0200

    Muestra hasta 5 coches en la página principal. Solo hay 1, se verá 1.

--- taller/urls.py
+++ taller/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
+
 urlpatterns = [
     url(r'^', include('gestion.urls')),
-    url(r'^admin/',include(admin.site.urls))
+    url(r'^admin/',include(admin.site.urls)),
+
 ]
